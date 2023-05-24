5d4d87b3b450b9ee252b9d7e8cb7e63bd3599c9f
kitakou0313 <kitakou0313@gmail.com>
2023-05-24 14:34:33 +0000

    Define echo server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func helloWorldHandler(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, world")
+}
+
 func main() {
-	print("Hello, world!")
+	e := echo.New()
+
+	e.GET("/", helloWorldHandler)
+
+	e.Logger.Fatal(e.Start(":8080"))
+
 }
