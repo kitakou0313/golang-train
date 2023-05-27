b6b9bb4860770743ed39bd578f4ad9785870aeb1
kitakou0313 <kitakou0313@gmail.com>
2023-05-27 03:41:48 +0000

    Add memo

--- cmd/designPattern/factory/main.go
+++ cmd/designPattern/factory/main.go
@@ -7,14 +7,14 @@ type Product interface {
 type ProductA struct {
 }
 
-func (pa *ProductA) doStuff() {
+func (pa ProductA) doStuff() {
 	println("Prodoct A")
 }
 
 type ProductB struct {
 }
 
-func (pb *ProductB) doStuff() {
+func (pb ProductB) doStuff() {
 	println("Prodoct B")
 }
 
@@ -25,24 +25,47 @@ type Creater interface {
 // creatorの責務はインスタンスの生成では無くビジネスロジックの管理
 
 type CreaterA struct {
+	cache *ProductA
 }
 
-func (cA *CreaterA) createProduct() Product {
-	return &ProductA{}
+func (cA CreaterA) createProduct() Product {
+	if cA.cache != nil {
+		return cA.cache
+	}
+
+	cA.cache = &ProductA{}
+
+	return cA.cache
 }
 
 type CreaterB struct {
+	cache *ProductB
 }
 
-func (cB *CreaterB) createProduct() Product {
-	res := ProductB{}
-	return &res
+func (cB CreaterB) createProduct() Product {
+	if cB.cache != nil {
+		return cB.cache
+	}
+
+	cB.cache = &ProductB{}
+	return cB.cache
 }
 
 func main() {
+	/*
+		Factory pattern
+		creational patternの一種
+
+		インスタンスを生成するFactoryのスーパークラスについてinterfaceを定義し，
+		実際のインスタンスの生成はサブクラスに行わせて生成するインスタンスの型を変化させるデザインパターン
+		本質はcreatorの方
+
+		同じインターフェースで取り扱いたいけど，内部のビジネスロジックは異なる　みたいなパターンで有効そう
+
+		Factoryクラスが生成したインスタンス（product）をclientが使う（内部の振る舞いについては関知しない）イメージっぽいわね
+	*/
 	createrB := CreaterB{}
 	productB := createrB.createProduct()
-
 	createrA := CreaterA{}
 	productA := createrA.createProduct()
 
