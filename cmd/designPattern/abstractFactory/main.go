22e73c44dc8d6635c8597c7ea3559ac913c5162b
kitakou0313 <kitakou0313@gmail.com>
2023-06-02 13:49:50 +0000

    Implement abstract factory methods

--- cmd/designPattern/abstractFactory/main.go
+++ cmd/designPattern/abstractFactory/main.go
@@ -1,16 +1,52 @@
 package main
 
 type IShoe interface {
-	setMark(logo string)
+	getMark() string
 }
 
 type IShirt interface {
-	setMark(logo string)
+	getMark() string
 }
 
 type ISportsFactory interface {
 	makeShoe() IShoe
 	makeShirt() IShirt
+}
+
+type AMakerShoe struct {
+	logo string
+}
+
+func (as *AMakerShoe) getMark() string {
+	return as.logo
+}
+
+type AMakerGoodsFactory struct {
+	logo string
+}
+
+func (amf *AMakerGoodsFactory) makeShoe() IShoe {
+	return &AMakerShoe{
+		logo: amf.logo,
+	}
+}
+
+type BMakerShoe struct {
+	logo string
+}
+
+func (bs *BMakerShoe) getMark() string {
+	return bs.logo
+}
+
+type BMakerGoodsFactory struct {
+	logo string
+}
+
+func (bmf *BMakerGoodsFactory) makeShoe() IShoe {
+	return &BMakerShoe{
+		logo: bmf.logo,
+	}
 }
 
 func main() {
