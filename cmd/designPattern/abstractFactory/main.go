9192a36961039f7b6b1ddf26152717f0ff99e3d9
kitakou0313 <kitakou0313@gmail.com>
2023-06-03 14:23:39 +0000

    Add memo about abstruct factory method

--- cmd/designPattern/abstractFactory/main.go
+++ cmd/designPattern/abstractFactory/main.go
@@ -8,6 +8,7 @@ type IShirt interface {
 	getMark() string
 }
 
+// 複数のproductの生成を行うクラスのinterfaceを定義し，生成するvariation毎にfactoryサブクラスを定義する
 type ISportsFactory interface {
 	makeShoe() IShoe
 	makeShirt() IShirt
@@ -21,12 +22,26 @@ func (as *AMakerShoe) getMark() string {
 	return as.logo
 }
 
+type AMakerShirt struct {
+	logo string
+}
+
+func (asi *AMakerShirt) getMark() string {
+	return asi.logo
+}
+
 type AMakerGoodsFactory struct {
 	logo string
 }
 
 func (amf *AMakerGoodsFactory) makeShoe() IShoe {
 	return &AMakerShoe{
+		logo: amf.logo,
+	}
+}
+
+func (amf *AMakerGoodsFactory) makeShirt() IShirt {
+	return &AMakerShirt{
 		logo: amf.logo,
 	}
 }
@@ -39,6 +54,14 @@ func (bs *BMakerShoe) getMark() string {
 	return bs.logo
 }
 
+type BMakerShirt struct {
+	logo string
+}
+
+func (bsi *BMakerShirt) getMark() string {
+	return bsi.logo
+}
+
 type BMakerGoodsFactory struct {
 	logo string
 }
@@ -49,6 +72,28 @@ func (bmf *BMakerGoodsFactory) makeShoe() IShoe {
 	}
 }
 
+func (bmf *BMakerGoodsFactory) makeShirt() IShirt {
+	return &BMakerShirt{
+		logo: bmf.logo,
+	}
+}
+
 func main() {
 
+	arg := "B"
+
+	var factory ISportsFactory
+
+	if arg == "A" {
+		factory = &AMakerGoodsFactory{
+			logo: "A",
+		}
+	} else {
+		factory = &BMakerGoodsFactory{
+			logo: "B",
+		}
+	}
+
+	shoe := factory.makeShoe()
+	shirt := factory.makeShirt()
 }
