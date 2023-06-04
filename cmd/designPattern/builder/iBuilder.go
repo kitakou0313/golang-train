17bf15b218fb8dab921f71a1af4a0dfa7422e09e
kitakou0313 <kitakou0313@gmail.com>
2023-06-04 05:51:57 +0000

    Define product and builder class

---
+++ cmd/designPattern/builder/iBuilder.go
@@ -0,0 +1,21 @@
+package main
+
+type IBuilder interface {
+	setWindowType()
+	setDoorType()
+	setNumFloor()
+	getHouse() House
+}
+
+func getBuilder(builderType string) IBuilder {
+	// ToDo 初期化用コード書く
+
+	if builderType == "normal" {
+		return newNormalBuilder()
+	}
+
+	if builderType == "igloo" {
+		return newIglooBuilder()
+	}
+	return nil
+}
